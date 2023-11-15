65479723f0e865204e6c624565faba65fed543cf
Motalib Hossain <motalibhossainrbk33@gmail.com>
2023-11-16 00:26:27 +0600

    update dynamic url setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import vedioFetchSlice from '../features/Videos/VideoSlice'
+import TagsSlice from '../features/Tags/TagsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    vedios:vedioFetchSlice
+    vedios:vedioFetchSlice,
+    tags:TagsSlice,
   },
 });
