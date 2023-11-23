7325f8ba32c12800c05a27a0b6ea8e25d34d6052
Motalib Hossain <motalibhossainrbk33@gmail.com>
2023-11-24 01:40:30 +0600

    update related tags.

--- src/features/relatedVedios/relatedVedioSlice.js
+++ src/features/relatedVedios/relatedVedioSlice.js
@@ -2,46 +2,23 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    vedios: [],
-    isLoading: false,
-    isError: false,
-    errorMessage: ""
+    relatedTags: [],
+    search: ""
 };
-
-// -----------------------------------------
-// 	create async-thank request
-// -----------------------------------------
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-const vedioUrl = `${SERVER_URL}/videos`;
-
-export const fetchVedios = createAsyncThunk("vedios/fetchVedios", async () => {
-    const response = await axios.get(vedioUrl);
-    return response?.data;
-});
 
 // -----------------------------------------
 // 	create slice
 // -----------------------------------------
 
-const vedioFetchSlice = createSlice({
-    name: "vedios",
+const relatedVediosSlice = createSlice({
+    name: "relatedTags",
     initialState,
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchVedios.pending, (state, action) => {
-                state.isLoading = "True";
-            })
-            .addCase(fetchVedios.fulfilled, (state, action) => {
-                state.isLoading = "False";
-                state.vedios = action.payload;
-                console.log("ok", SERVER_URL);
-            })
-            .addCase(fetchVedios.rejected, (state, action) => {
-                state.isLoading = "False"
-                state.isError = "True";
-                state.errorMessage = action.payload;
-            });
+    reducers: {
+        addTags: (state, action) => {
+            state.relatedTags.push(action.payload);
+        }
     }
 });
 
-export default vedioFetchSlice.reducer;
+export const { addTags } = relatedVediosSlice.actions;
+export default relatedVediosSlice.reducer;
