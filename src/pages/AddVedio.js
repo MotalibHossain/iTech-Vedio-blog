1cb9bebb20242597de3900268aaa40ada5ca8ee6
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2023-12-19 09:58:14 +0000

    update responsive

--- src/pages/AddVedio.js
+++ src/pages/AddVedio.js
@@ -4,15 +4,29 @@ export default function AddVedio() {
     return (
         <>
             <section className="mb-8 mt-5">
-                <div className="container mx-auto mx-auto">
-                    <form action="">
-                        <div className="grid grid-rows">
-							<label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">Title</label>
-							<div>
-								<input type="text" name="title" className="h-full rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"/>
-							</div>
-						</div>
-                    </form>
+                <div className="container mx-auto">
+                    <div className="max-w-7xl p-6 mx-auto bg-slate-200 rounded-md shadow-md dark:bg-gray-800 mt-20">
+                        <form action="">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-4">
+                                <div className="grid">
+                                    <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900 mb-2">Title</label>
+                                    <input type="text" name="title" className="w-full h-full px-4 py-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"/>
+                                </div>
+                                <div className="grid">
+                                    <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900 mb-2">Title</label>
+                                    <input type="text" name="title" className="w-full h-full px-4 py-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"/>
+                                </div>
+                                <div className="grid">
+                                    <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900 mb-2">Title</label>
+                                    <input type="text" name="title" className="w-full h-full px-4 py-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"/>
+                                </div>
+                                <div className="grid">
+                                    <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900 mb-2">Title</label>
+                                    <input type="text" name="title" className="w-full h-full px-4 py-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"/>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </section>
         </>
