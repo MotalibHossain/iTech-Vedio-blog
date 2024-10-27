c9f76c5bcb5976c1aff8f1fe08cb91e007bbe059
MotalibHossain <motalibhossainrbk33@gmail.com>
2024-10-27 22:34:33 +0600

    update modal functionality.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { createBlogPost } from '../features/blogPostSlice';
+import UpdateProductModal from "../Components/updateProductModal";
+import { createBlogPost } from "../features/blogPostSlice";
 
 const Home = () => {
     const dispatch = useDispatch();
-    const {post, isLoading, isError} = useSelector(
-        (state)=> state.post
-    )
-    console.log("Posts =", post, isError, isLoading)
+    const { post, isLoading, isError } = useSelector((state) => state.post);
+    console.log("Posts =", post, isError, isLoading);
 
     useEffect(() => {
         dispatch(createBlogPost());
     }, [dispatch]);
-
-
 
     return (
         <main>
@@ -363,9 +360,12 @@ const Home = () => {
                                         <th scope="col" className="px-4 py-3">
                                             Title
                                         </th>
-                                        <th scope="col" className="px-4 py-3 truncate">
+                                        <th
+                                            scope="col"
+                                            className="px-4 py-3 truncate"
+                                        >
                                             Description
-                                        </th>                               
+                                        </th>
                                         <th scope="col" className="px-4 py-3">
                                             <span className="sr-only">
                                                 Actions
@@ -374,22 +374,31 @@ const Home = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {post && post.map((item, index)=>{
-                                        const {id, userId, title, body } = item;
-                                        return(
-                                            <>
-                                                <tr key={index} className="border-b dark:border-gray-700">
+                                    {post &&
+                                        post.map((item, index) => {
+                                            const { id, userId, title, body } =
+                                                item;
+                                            return (
+                                                <tr
+                                                    key={index}
+                                                    className="border-b dark:border-gray-700"
+                                                >
                                                     <th
                                                         scope="row"
                                                         className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                                                     >
                                                         {id}
                                                     </th>
-                                                    <td className="px-4 py-3">{userId}</td>
-                                                    <td className="px-4 py-3">{title}</td>
+                                                    <td className="px-4 py-3">
+                                                        {userId}
+                                                    </td>
+                                                    <td className="px-4 py-3">
+                                                        {title}
+                                                    </td>
                                                     <td className="px-4 py-3 max-w-[12rem] truncate">
                                                         {body}
                                                     </td>
+
                                                     <td className="px-4 py-3 flex items-center justify-end">
                                                         <button
                                                             id={`apple-imac-27-dropdown-button-${index}`}
@@ -418,8 +427,8 @@ const Home = () => {
                                                                 <li>
                                                                     <button
                                                                         type="button"
-                                                                        data-modal-target="updateProductModal"
-                                                                        data-modal-toggle="updateProductModal"
+                                                                        data-modal-target={`updateProductModal-${index}`}
+                                                                        data-modal-toggle={`updateProductModal-${index}`}
                                                                         className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
                                                                     >
                                                                         <svg
@@ -437,30 +446,6 @@ const Home = () => {
                                                                             />
                                                                         </svg>
                                                                         Edit
-                                                                    </button>
-                                                                </li>   
-                                                                <li>
-                                                                    <button
-                                                                        type="button"
-                                                                        data-modal-target={`readProductModal-${index}`}
-                                                                        data-modal-toggle={`readProductModal-${index}`}
-                                                                        className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
-                                                                    >
-                                                                        <svg
-                                                                            className="w-4 h-4 mr-2"
-                                                                            xmlns="http://www.w3.org/2000/svg"
-                                                                            viewBox="0 0 20 20"
-                                                                            fill="currentColor"
-                                                                            aria-hidden="true"
-                                                                        >
-                                                                            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                                            <path
-                                                                                fillRule="evenodd"
-                                                                                clipRule="evenodd"
-                                                                                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                                                            />
-                                                                        </svg>
-                                                                        Preview
                                                                     </button>
                                                                 </li>
                                                                 <li>
@@ -490,10 +475,11 @@ const Home = () => {
                                                             </ul>
                                                         </div>
                                                     </td>
+
+                                                    <UpdateProductModal  index={index} />
                                                 </tr>
-                                            </>
-                                        )
-                                    })}
+                                            );
+                                        })}
                                 </tbody>
                             </table>
                         </div>
@@ -602,6 +588,7 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+
             <div
                 id="createProductModal"
                 tabIndex="-1"
@@ -745,116 +732,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            
-            <div
-                id="readProductModal-0"
-                tabIndex="-1"
-                aria-hidden="true"
-                className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-            >
-                <div className="relative p-4 w-full max-w-xl max-h-full">
-                    <div className="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                        <div className="flex justify-between mb-4 rounded-t sm:mb-5">
-                            <div className="text-lg text-gray-900 md:text-xl dark:text-white">
-                                <h3 className="font-semibold ">
-                                    Apple iMac 27”
-                                </h3>
-                                <p className="font-bold">$2999</p>
-                            </div>
-                            <div>
-                                <button
-                                    type="button"
-                                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white"
-                                    data-modal-toggle="readProductModal-0"
-                                >
-                                    <svg
-                                        aria-hidden="true"
-                                        className="w-5 h-5"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                    <span className="sr-only">Close modal</span>
-                                </button>
-                            </div>
-                        </div>
-                        <dl>
-                            <dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">
-                                Details
-                            </dt>
-                            <dd className="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">
-                                Standard glass ,3.8GHz 8-core 10th-generation
-                                Intel Core i7 processor, Turbo Boost up to
-                                5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro
-                                5500 XT with 8GB of GDDR6 memory, 256GB SSD
-                                storage, Gigabit Ethernet, Magic Mouse 2, Magic
-                                Keyboard - US.
-                            </dd>
-                            <dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">
-                                Category
-                            </dt>
-                            <dd className="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">
-                                Electronics/PC
-                            </dd>
-                        </dl>
-                        <div className="flex justify-between items-center">
-                            <div className="flex items-center space-x-3 sm:space-x-4">
-                                <button
-                                    type="button"
-                                    className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                                >
-                                    <svg
-                                        aria-hidden="true"
-                                        className="mr-1 -ml-1 w-5 h-5"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                    Edit
-                                </button>
-                                <button
-                                    type="button"
-                                    className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                                >
-                                    Preview
-                                </button>
-                            </div>
-                            <button
-                                type="button"
-                                className="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
-                            >
-                                <svg
-                                    aria-hidden="true"
-                                    className="w-5 h-5 mr-1.5 -ml-1"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                                Delete
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
             <div
                 id="deleteModal-0"
                 tabIndex="-1"
